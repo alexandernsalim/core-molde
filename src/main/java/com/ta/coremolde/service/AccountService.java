b4413b54c5e7fa0dfa0f7d83616cf69bb14b12d7
Alexander Nicholas Salim <alexandernsalim@gmail.com>
2020-04-02 16:03:25 +0700

    Fix import error on AccountService class

--- src/main/java/com/ta/coremolde/service/AccountService.java
+++ src/main/java/com/ta/coremolde/service/AccountService.java
@@ -1,8 +1,8 @@
 package com.ta.coremolde.service;
 
-import com.ta.coremolde.model.entity.Account;
-import com.ta.coremolde.model.request.AccountRequest;
-import com.ta.coremolde.model.response.AccountResponse;
+import com.ta.coremolde.master.model.entity.Account;
+import com.ta.coremolde.master.model.request.AccountRequest;
+import com.ta.coremolde.master.model.response.AccountResponse;
 
 public interface AccountService {
     Account getAccount(String email);
